b616d074aab9df9f7bf8514a615950f79befca22
Diogo Soares <diogo.soares05@gmail.com>
2025-08-27 08:34:43 +0200

    added sender and receiver to main.go

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"time"
+
 	"github.com/DSoares08/Phantom/network"
 )
 
 func main() {
 	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+			trLocal.SendMessage(trRemote.Addr(), []byte("hello world"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
 
 	opts := network.ServerOpts{
 		Transports: []network.Transport{trLocal},
